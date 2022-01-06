e1da480aee05dede2e1c55ce2f9450056e3588e4
mrstraining <39810269+amarprs@users.noreply.github.com>
2022-01-06 12:23:22 +0000

    Add project files.

---
+++ D01_ForStudents/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace D01_ForStudents
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Milena");
+
+            // TODO MRS: cada um de vocês deve acrescentar aqui o seu console.writeline
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
